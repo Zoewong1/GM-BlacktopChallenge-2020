83513dc3569a81637aa8a3d834ad87ff5ffc6c87
Suleman Thaniana <msuleman@mit.edu>
2020-11-14 17:46:46 -0500

    final

--- chicagocrashes.py
+++ chicagocrashes.py
@@ -4,17 +4,24 @@ locations = {}
 with open('../chicagocrashes.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
+        try:
+            lat = float(row['LATITUDE'])
+            lon = float(row['LONGITUDE'])
+        except:
+            continue
         if '2019' in row['CRASH_DATE'] or '2020' in row['CRASH_DATE']:
             if 'DARKNESS' not in row['LIGHTING_CONDITION']:
-                if row['STREET_NAME'] in data:
-                    data[row['STREET_NAME']] += 1
-                else:
-                    locations[row['STREET_NAME']] = row['LOCATION']
-                    data[row['STREET_NAME']] = 1
+                if 'ONE-WAY' not in row['TRAFFICWAY_TYPE']:
+                    if 'PARKED MOTOR VEHICLE' not in row['FIRST_CRASH_TYPE']:
+                        if 'TRAFFIC SIGNAL' in row['TRAFFIC_CONTROL_DEVICE']:
+                            if (round(lat,4),round(lon,4),) in data:
+                                data[(round(lat,4),round(lon,4))] += 1
+                            else:
+                                data[(round(lat,4),round(lon,4),)] = 1
 
 sorted_data = sorted(data.items(), key= lambda x:x[1],reverse = True)
 first_10 = sorted_data[:10]
 for i in first_10:
-    print('intersection: ', i[0])
+    print('location ', i[0])
     print('crashes: ', i[1])
-    print('location: ', locations[i[0]])
+    # print('location: ', locations[i[0]])
