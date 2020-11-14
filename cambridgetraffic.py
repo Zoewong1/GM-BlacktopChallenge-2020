fd9862e22691d52dbd65533e5bf4210f54c89db0
Suleman Thaniana <msuleman@mit.edu>
2020-11-14 14:22:40 -0500

    t

--- cambridgetraffic.py
+++ cambridgetraffic.py
@@ -5,7 +5,7 @@ with open('camtraffic.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file,['x','y','id','count','intnum','location','dis'])
     for row in csv_reader:
         d[row['id']] = (row['x'],row['y'],row['count'],row['intnum'],row['location'],row['dis'])
-print(d)
+print(d['1'])
         
 
 
